e1a8d37201ce8c59cd508780856c0c1b25cb5386
Anshuman Tiwari <anshumantiwari21153045@gmail.com>
2024-12-09 20:57:35 +0530

    fic: job name fixed

--- app/generator/job_generator.py
+++ app/generator/job_generator.py
@@ -1,17 +1,17 @@
-from faker import Faker
 from sqlalchemy.orm import Session
 from app.models.job_model import Job
 from constants import COMMON_COLUMNS
+import random
+import string
 
-
-fake = Faker()
 
 def create_job(session: Session, all_tasks:list[str],type:str)->list[list[str]]:
 
     all_task_id=[]
     for task_id in all_tasks:
+        name = ''.join(random.choices(string.ascii_lowercase, k=4))
         job=Job(
-            name=fake.sentence(nb_words=4).rstrip('.'),
+            name=name,
             meta={},
             comparison_id=task_id if (type=="comparison") else None,
             validation_id=task_id if (type=="validation") else None,
